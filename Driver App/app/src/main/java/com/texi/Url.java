c6707cad9ee72a3718411dac7b158f2d8e3fa302
@AshishChatterjee <ashishchatterjee139@gmail.com>
2018-12-18 17:51:50 +0530

    intitial upload

---
+++ Driver App/app/src/main/java/com/texi/Url.java
@@ -0,0 +1,32 @@
+package com.texi;
+
+/**
+ * Created by bits
+ */
+public class Url {
+
+    //public static String baseUrl = "http://138.68.5.43/web_service/";
+    public static String baseUrl = "http://www.mediaorange.co/olauber/web_service/";
+    public static String driver_sign_up = baseUrl+"driver_sign_up";
+    public static String driver_edit_profile = baseUrl+"driver_profile_edit";
+    public static String driver_login = baseUrl+"driver_login?";
+    public static String driver_forgot_password = baseUrl+"driver_forgot_password?";
+    public static String DriverTripUrl = baseUrl+"driver_bookings";
+    public static String DriverAcceptTripUrl = baseUrl+"driver_accept_trip";
+    public static String DriverRejectTripUrl = baseUrl+"driver_reject_trip";
+    public static String DriverFilterTripUrl = baseUrl+"driver_filter_book";
+
+    public static String DriverArrivedTripUrl = baseUrl+"driver_arrived_trip";
+    public static String DriverOnTripUrl = baseUrl+"driver_on_trip";
+    public static String DriverCompletedTripUrl = baseUrl+"driver_completed_trip";
+    public static String DriverChangPasswordUrl = baseUrl+"driver_change_password";
+
+    public static String driver_cartype = baseUrl+"car_type";
+    public static String imageurl="http://www.mediaorange.co/olauber/driverimages/";
+    public static String userImageUrl = "http://www.mediaorange.co/olauber/user_image/";
+    public static String subscribeUrl = "http://www.mediaorange.co/olauber/subscribe";
+    public static String unsubscribeUrl = "http://www.mediaorange.co/olauber/unsubscribe";
+    public static String carImageUrl = "http://www.mediaorange.co/olauber/car_image/";
+
+    public static String FacebookImgUrl = "https://graph.facebook.com/";
+}
